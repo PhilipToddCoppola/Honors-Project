0fc9422d49fbe05c72270cb161e245d9f5a221f1
PhilipToddCoppola <p.todd@dundee.ac.uk>
2014-12-01 16:13:50 +0000

    Cell Size Confirmation
    Added a function in my Root_Model.py code that would print the length of
    the cell to a Data file which I could use to produce a Plot of cell
    length.
    
    Noticed error with my division rate so have altered that and appended
    the correction to the Root_Model.py script.

--- ROOT/Main Code/Division_Rate_Check.py
+++ ROOT/Main Code/Division_Rate_Check.py
@@ -9,17 +9,17 @@ import math
 
 BI_max = 720.       #Bilinear Inerpolation Max value
 BI_min = 410.       #Bilinear Interpolation Min Value
-Init_DR = 1./0.04   #Division Rate(dr) per Hour
+Init_DR = 0.04      #Division Rate(dr) per cell per hour
 
 
 def divRate(x):
     if x < 410:
         dr = Init_DR
         return dr
-    if x >= 410 and x <= 720:
+    if x >= 410 and x <= 715:
         dr = (BI_max - x)/(BI_max-BI_min) *(Init_DR) #Bilinear Interpolation
         return dr
-    if x > 720:
+    if x > 715:
         dr = 0.
         return dr
 
@@ -28,7 +28,7 @@ V = []
 for x in X:
     V.append(divRate(x))
 plt.plot(X,V)
-plt.axis([0,1000, -5, 30])
-plt.ylabel('Division Rate per cell')
+plt.axis([0,1000, -0.01, 0.05])
+plt.ylabel('Division Rate per cell per hour')
 plt.xlabel('Distance from Quiescent Center (um)')
 plt.show()
